42ad8a2bb8949b096b82fbcd73395e224830cada
Someone1121 <eli.tribhuwan12@gmail.com>
2025-06-29 14:33:01 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("sup baby");
+
+
+// window.alert("")  use this to makenn a popup window at the top 
+
+
+let use
+
+document.getElementById("myH1").textContent = "headers header waht "
+
+document.getElementById("MyP").textContent = "sigma sigma boy on the wall"
+
+document.getElementById("button").onclick = function(){
+
+use = document.getElementById("input").value;
+document.getElementById("myH1").textContent = "sup " + use
+console.log(use);
+
+}
+
+// when you get inout it is a string automaticlly so set that vairable = Number(the variable) / Boolean(the variable) to change it
